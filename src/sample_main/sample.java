8674eaf36d4ed196af96ba78dd2edd40516ea1a6
荒木　信哉 <shinnya@MacBook-Pro.local>
2020-12-08 21:47:34 +0900

    [add]post

--- src/sample_main/sample.java
+++ src/sample_main/sample.java
@@ -28,11 +28,13 @@ public class sample extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		request.setAttribute("userName", "Guest");
+	    String name = (String) request.getAttribute("userName");
+
+	    if (name == null || "".equals(name)) {
+	        request.setAttribute("userName", "Guest");
+	    }
 		String view = "/WEB-INF/view/sample.jsp";
 	    RequestDispatcher dispatcher = request.getRequestDispatcher(view);
-
 	    dispatcher.forward(request, response);
 	}
 
@@ -40,9 +42,12 @@ public class sample extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    // 文字コードの指定
+	    request.setCharacterEncoding("utf-8");
 	    // formから値を取得
-	    String value = request.getParameter("hoge");
-	    System.out.println(value);
+	    String name = request.getParameter("name");
+
+	    request.setAttribute("userName", name);
 
 	    doGet(request, response);
 	}
